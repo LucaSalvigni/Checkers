231f4a6a97e31ea1f5bd945ff1605feb0f61c257
LucaSalvigni <salvigni.luca97@gmail.com>
2022-04-19 11:21:05 +0200

    Add new dipendencies to connect frontend with backend

--- Frontend/Checkers-Frontend/src/main.js
+++ Frontend/Checkers-Frontend/src/main.js
@@ -1,10 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueSocketIO from "vue-3-socket.io";
+import SocketIO from "socket.io-client";
 import store from "./store";
 import Chat from "vue3-beautiful-chat";
 import "./index.css";
 import "@fortawesome/fontawesome-free/js/all";
+
+const connection = SocketIO("http://localhost:3030");
 
 export const app = createApp(App);
 app.config.productionTip = false;
@@ -15,4 +19,14 @@ app.config.globalProperties.$BUTTON_CLICK = new Audio(
   require("@/assets/sounds/button-click.wav")
 );
 
-app.use(Chat).use(store).use(router).mount("#app");
+app
+  .use(
+    new VueSocketIO({
+      connection: connection,
+      vuex: store,
+    })
+  )
+  .use(Chat)
+  .use(store)
+  .use(router)
+  .mount("#app");
