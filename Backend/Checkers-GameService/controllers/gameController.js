378e7e604e722f83bc63c1e23aec9c7e3b380483
Konrad Gomulka <konrad.gomulka@studio.unibo.it>
2022-04-22 18:15:07 +0200

    Add leaveGame function

--- Backend/Checkers-GameService/controllers/gameController.js
+++ Backend/Checkers-GameService/controllers/gameController.js
@@ -3,6 +3,8 @@ const { log } = require('../../Utils/utils');
 const Game = require('../models/gameModel');
 
 const games = new Map();
+const winStars = 100;
+const lossStars = -70;
 
 exports.create_game = function (req, res) {
   try {
@@ -67,9 +69,46 @@ async function gameEnd(gameId, tie, winner, loser) {
 }
 
 exports.tieGame = function (req, res) {
-  gameEnd(req.body.game_id, true, _, _).then(
+  gameEnd(req.body.gameId, true, _, _).then(
     res.status(200).send({ message: 'Game has been settled with a tie, each player will not earn nor lose stars' }),
   ).catch(
     res.status(500).send({ message: 'Something went wrong while closing the game.' }),
   );
+};
+
+/**
+ * Handles user leaving a game
+ */
+exports.leaveGame = async function (req, res) {
+  const { gameId } = req.body;
+  const quitter = req.body.player_id;
+  try {
+    if (games.has(gameId)) {
+      log(`${quitter} is leaving game ${gameId}`);
+      const game = games.get(gameId);
+      if (game.white === quitter) {
+        log(`${quitter} is the host of game ${gameId}`);
+        await gameEnd(gameId, false, game.black, game.white);
+      } else if (game.black === quitter) {
+        log(`${quitter} is not the host of game ${gameId}`);
+        await gameEnd(gameId, false, game.white, game.black);
+      } else {
+        log(`Wat apparently ${quitter} has nothing to do with this game`);
+        res.status(400).send({ message: `${quitter} is not in any game` });
+        return;
+      }
+
+      const data = [];
+      data.push(`You successfully left the game!\n ${winStars} stars have been removed from your profile!`);
+      data.push(`The opponent has left the game!\n ${lossStars} stars have been added to your profile`);
+      res.status(200).send(data);
+    } else {
+      log(`There is no such thing as game ${gameId}`);
+      res.status(400).send({ message: 'There is no such game' });
+    }
+  } catch (err) {
+    log(`Something wrong while processing ${quitter} request of leaving game ${gameId}`);
+    log(err);
+    res.status(500).send({ message: 'Internal server error while leaving game' });
+  }
 };
