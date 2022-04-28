a4179aed87deaa4909e54ff1bd97831dd672915d
Manuele Pasini <manuele.pasini@studio.unibo.it>
2022-04-28 09:01:00 +0200

    Certificate verification
    Implemented certificate verification in each service

--- Backend/Checkers-GameService/routes/routes.js
+++ Backend/Checkers-GameService/routes/routes.js
@@ -4,6 +4,20 @@ const router = express.Router();
 const gameController = require('../controllers/gameController');
 
 router
+// Certificate verification
+.all("*", function(req,res,next){
+  const cert = req.socket.getPeerCertificate();
+  if (req.client.authorized) {
+    next()
+  } else if (cert.subject) {
+    res.status(403)
+      .send(`Sorry ${cert.subject.CN}, can't accept certificates from ${cert.issuer.CN}.`);
+
+  } else {
+    res.status(401)
+      .send(`Sorry, need to provide a valid certificate.`);
+  }
+})
   .post('/game/lobbies/create_game', gameController.create_game)
   .put('/game/tieGame', gameController.tieGame)
   .delete('/game/leaveGame', gameController.leaveGame);
