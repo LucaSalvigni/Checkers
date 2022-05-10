baa8bd92c948b443e2ed4b0d81f508e360a30c56
LucaSalvigni <salvigni.luca97@gmail.com>
2022-05-10 11:25:33 +0200

    Debugging

--- Backend/Checkers-CommunicationService/test/utils/api.js
+++ Backend/Checkers-CommunicationService/test/utils/api.js
@@ -7,7 +7,7 @@ require('../../index');
 // Require the dev-dependencies
 // chai.use(chaiHttp);
 // chai.should();
-//Just a comment for debug
+// Debug
 
 let clientSocket = new Client('http://:3030');
 
